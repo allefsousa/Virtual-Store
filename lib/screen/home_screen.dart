c4c6d4d73104f79e118bc6bb3b96bc36ca916c71
Allef Sousa <allefsousa_1@hotmail.com>
2020-05-18 00:43:44 -0300

    Criando items de do menu drawer

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/tabs/home_tab.dart';
+import 'package:lojavirtual/widget/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   final _pagerController = PageController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +12,9 @@ class HomeScreen extends StatelessWidget {
       controller: _pagerController ,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        HomeTab(),
-        Container(color: Colors.yellow,),
-        Container(color: Colors.red,),
-        Container(color: Colors.blue,),
-        Container(color: Colors.black,)
+        Scaffold(
+          body:HomeTab(), drawer: CustomDrawer()
+        ),
       ],
     );
   }
