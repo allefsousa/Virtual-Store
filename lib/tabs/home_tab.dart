f5a59cda6c7ba60a1c598c8433c020d58ce94c05
Allef Sousa <allefsousa_1@hotmail.com>
2020-05-18 00:10:45 -0300

    Criando stagerred Grid com firestore

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -30,7 +34,45 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Novidades"),
                 centerTitle: true,
               ),
-            )
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance.collection("home")
+                  .orderBy("pos").getDocuments(),
+              builder: (context,snashot){
+                if(!snashot.hasData)
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                else{
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snashot.data.documents.map(
+                        (doc){
+                          return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                        }
+                    ).toList(),
+                    children: snashot.data.documents.map(
+                        (doc){
+                          return FadeInImage.assetNetwork(
+                              fit: BoxFit.cover,
+                              placeholder: kTransparentImage.toString(),
+                              image: doc.data["image"]
+                          );
+                        }
+                    ).toList(),
+                  );
+
+                }
+              },
+            ),
           ],
         )
       ],
