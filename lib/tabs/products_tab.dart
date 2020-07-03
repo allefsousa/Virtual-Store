ac12b738d29e06fc8be3bed2ba094338122ff77e
Allef Sousa <allefsousa_1@hotmail.com>
2020-07-02 22:59:42 -0300

    Criando Dividers na lista

--- lib/tabs/products_tab.dart
+++ lib/tabs/products_tab.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/tiles/category_tille.dart';
 
 class ProductsTab extends StatefulWidget {
   @override
@@ -11,15 +12,23 @@ class _ProductsTabState extends State<ProductsTab> {
   Widget build(BuildContext context) {
     return FutureBuilder<QuerySnapshot>(
       future: Firestore.instance.collection("products").getDocuments(),
-      builder: (context,snapshot){
+      builder: (context, snapshot) {
         if (!snapshot.hasData)
-          return Center(child: CircularProgressIndicator(),);
-        else
-          return ListView(
-            children: <Widget>[
-
-            ],
+          return Center(
+            child: CircularProgressIndicator(),
           );
+        else{
+          var divideTiles = ListTile.divideTiles(
+              tiles: snapshot.data.documents.map((doc) {
+                return CategoryTile(doc);
+              }).toList(),
+              color: Colors.grey[500])
+              .toList();
+
+          return ListView(
+            children: divideTiles,
+          );
+        }
       },
     );
   }
