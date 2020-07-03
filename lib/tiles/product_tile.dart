a5602b642683932418cb2b5b41d9d2042e058fd2
Allef Sousa <allefsousa_1@hotmail.com>
2020-07-03 00:18:12 -0300

    Criando estrutura de listagem em grid em em lista

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/data/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+
+  final String type;
+  final ProductData productData;
+  ProductTile(this.type, this.productData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
