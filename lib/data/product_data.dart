d02af84c8781dcc84e396e8757c1a0cb2a48d4bd
Allef Sousa <allefsousa_1@hotmail.com>
2020-07-02 23:31:50 -0300

    Criando classe para receber os dados do firebase e converter

---
+++ lib/data/product_data.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductData {
+  String id;
+
+  String title;
+  String category;
+  String description;
+  double price;
+
+  List images;
+  List sizes;
+
+  ProductData.fromDocument(DocumentSnapshot snapshot){
+    id = snapshot.documentID;
+    title = snapshot.data["title"];
+    description = snapshot.data["description"];
+    price = snapshot.data["prices"];
+    images = snapshot.data["images"];
+    sizes = snapshot.data["sizes"];
+  }
+
+}
